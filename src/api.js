529a1e3325193020dc933fd9edaec4483cd3a8ca
Ben Greenberg <benjaming@gablit.com>
2015-11-02 08:59:12 -0800

    style consistency

--- src/api.js
+++ src/api.js
@@ -9,22 +9,32 @@ let $ = require('jquery')
 
 const apiURL = '/api'
 
+function login (username, password, callback) {
+  $.ajax(apiURL + '/login', {method: 'POST', dataType: 'json', data: {username, password}}).done(callback)
+}
+
+function signup (username, password, callback) {
+  $.ajax(apiURL + '/signup', {method: 'POST', dataType: 'json', data: {username, password}}).done(callback)
+}
+
+function addFavorite (favorite, callback) {
+  let data = _.pick(favorite, ['imdbID', 'Title', 'Year', 'Poster'])
+  console.log(data)
+  $.ajax(apiURL + '/favorites', {method: 'POST', dataType: 'json', data}).done(callback)
+}
+
+function getFavorites (username, callback) {
+  $.ajax(apiURL + '/favorites/' + username).done(callback)
+}
+
+function deleteFavorite (imdbID, callback) {
+  $.ajax(apiURL + '/favorites', {method: 'DELETE', dataType: 'json', data: {imdbID}}).done(callback)
+}
+
 module.exports = {
-  login (username, password, callback) {
-    $.ajax(apiURL + '/login', {method: 'POST', dataType: 'json', data: {username, password}}).done(callback)
-  },
-  signup (username, password, callback) {
-    $.ajax(apiURL + '/signup', {method: 'POST', dataType: 'json', data: {username, password}}).done(callback)
-  },
-  addFavorite (favorite, callback) {
-    let data = _.pick(favorite, ['imdbID', 'Title', 'Year', 'Poster'])
-    console.log(data)
-    $.ajax(apiURL + '/favorites', {method: 'POST', dataType: 'json', data}).done(callback)
-  },
-  getFavorites (username, callback) {
-    $.ajax(apiURL + '/favorites/' + username).done(callback)
-  },
-  deleteFavorite (imdbID, callback) {
-    $.ajax(apiURL + '/favorites', {method: 'DELETE', dataType: 'json', data: {imdbID}}).done(callback)
-  }
+  login,
+  signup,
+  addFavorite,
+  getFavorites,
+  deleteFavorite
 }
--- src/utils.js
+++ src/utils.js
@@ -7,36 +7,35 @@
 let _ = require('lodash')
 let api = require('./api')
 
-module.exports = {
-  render (markup, title) {
-    document.getElementById('content').innerHTML = markup
-    document.title = title
-  },
-  getCurrentUsername () {
-    let elem = document.getElementById('user-data')
-    return elem.dataset.username || ''
-  },
-  getCurrentFavorites () {
-    let favorites = document.getElementById('user-data').dataset.favorites
-    if (favorites === undefined) return []
-    return _.reduce(favorites.split(','), (last, curr) => {
-      last[curr] = 1
-      return last
-    }, {})
-  },
-  addFavorite (favorite, callback) {
-    console.log(favorite)
-    return function (ev) {
-      ev.preventDefault()
-      if (favorite) {
-        api.addFavorite(favorite, (rsp) => {
-          console.log(rsp)
-        })
-        callback()
-      }
+function render (markup, title) {
+  document.getElementById('content').innerHTML = markup
+  document.title = title
+}
+function getCurrentUsername () {
+  let elem = document.getElementById('user-data')
+  return elem.dataset.username || ''
+}
+function getCurrentFavorites () {
+  let favorites = document.getElementById('user-data').dataset.favorites
+  if (favorites === undefined) return []
+  return _.reduce(favorites.split(','), (last, curr) => {
+    last[curr] = 1
+    return last
+  }, {})
+}
+function addFavorite (favorite, callback) {
+  console.log(favorite)
+  return function (ev) {
+    ev.preventDefault()
+    if (favorite) {
+      api.addFavorite(favorite, (rsp) => {
+        console.log(rsp)
+      })
+      callback()
     }
-  },
-  deleteFavorite (imdbID, callback) {
+  }
+}
+function deleteFavorite (imdbID, callback) {
   return function (ev) {
     ev.preventDefault()
     if (imdbID) {
@@ -47,4 +46,12 @@ module.exports = {
     }
   }
 }
+
+module.exports = {
+  render,
+  getCurrentUsername,
+  getCurrentFavorites,
+  addFavorite,
+  deleteFavorite
 }
+
